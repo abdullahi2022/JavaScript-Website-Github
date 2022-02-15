a6b8cc37f8e37772893ed248cf2c0fd5a4638c40
abdullahi2022 <98919395+abdullahi2022@users.noreply.github.com>
2022-02-15 14:59:34 -0700

    update

--- Scripts/Max of Two Numbers.js
+++ Scripts/Max of Two Numbers.js
@@ -1,6 +1,24 @@
 console.log("You made it to the Max of Two Numbers.js!");
 //
+//Global Variables
 let stop = false;
+//
+function solution (number1, number2)
+{
+  if ( number1 == number2 ) return "  Equal";
+  // Ternary Operator Example (single comparison), needs single line IF to limit choices
+  return ( number1 > number2 ) ? "Number 2 is larger" : "Number 1 is larger";
+  //
+  /* Repeats the Ternary Operator
+  if ( number1 > number2 )
+  {
+    return "Number 1 is larger";
+  } else
+  {
+    return "Number 2 is larger";
+  }
+  */
+}//ENd solution
 //
 function main()
 {
@@ -8,15 +26,29 @@ function main()
   let secondNumber = document.getElementById("textField2").value;
   //document.getElementById("CanBeAnything").innerHTML = alert ("You are connected");
   document.getElementById("validityTest1").innerHTML = testNaN (firstNumber);
-  document.getElementById("validityTest1").innerHTML = testNaN (secondNumber);
+  document.getElementById("validityTest2").innerHTML = testNaN (secondNumber);
+  if (stop == true)
+  {
+    document.getElementById("answer").innerHTML = "Restart ... because I said so."
+  } else
+  {
+    document.getElementById("answer").innerHTML = "What did you say?  " + solution(firstNumber, secondNumber);
+  }//ENd stop=true
 }//End main()
 //
 function testNaN (number)
 {
+  if ( number=="" ) //Checking for null, submit-button misfire
+  //Variable returns emply string, not null
+  {
+    stop = true;
+    return "Please type a number into the textfield"
+  }//End null check
+  //
   if ( isNaN(number) ) //Returns a Boolean
   {
     stop = true;
-    return "Type a Real Number"
+    return "Type a Real Number";
   } else
   {
     return "Good. It is a number."
